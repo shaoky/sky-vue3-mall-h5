162e50d3e341caa8be95e8ed71601e6a8c41dfeb
shaoky <634207476@qq.com>
2022-07-16 10:41:33 +0800

    feat: 增加页面允许使用keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive"></component>
+      </keep-alive> 
+        <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive"></component>
+    </router-view>
   </div>
 </template>
 
